8c15c076e743de3f08c42c74376fdefdb3ec6432
natphotat <nattawatphotathong2547@gmail.com>
2026-02-09 17:26:27 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Nattawat"
+last_name = "Photathong"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Nattawat"
+last_name = "Photathong"
+whole_name = first_name + " " + last_name
+print(whole_name)
