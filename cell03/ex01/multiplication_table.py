47379a3e52004ce536c242d068ff07ff44598c2d
natphotat <nattawatphotathong2547@gmail.com>
2026-02-10 19:08:41 +0700

    Add files via upload

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+number = int(input("Enter the number: "))
+for i in range(0, 10):
+    result = i * number
+    print(f"{i} x {number} = {result}")
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+x = input(f"What you gotta say? : ")
+while x != "STOP":
+    x = input(f"I got that! Anything else? : ")
