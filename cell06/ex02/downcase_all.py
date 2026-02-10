457d4fbd43b71adb6543b3e5237f4928cbda959d
natphotat <nattawatphotathong2547@gmail.com>
2026-02-10 20:41:03 +0700

    Add files via upload

---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+def downcase_it(text):
+    return text.lower()
+
+if len(sys.argv) == 1:
+    print("none")
+
+else:
+    x = sys.argv[1:]
+    for i in x:
+        print(downcase_it(i))
---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def add_one(x):
+    x = x + 1
+
+y = 10
+
+print(y)
+add_one(y)
+print(y)
