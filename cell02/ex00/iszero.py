afb8630aefda00cc730a8db7a5c8c73a24362c37
natphotat <nattawatphotathong2547@gmail.com>
2026-02-10 19:02:37 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+user_input = input("Enter a number: ")
+number = int(user_input)
+if number == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
